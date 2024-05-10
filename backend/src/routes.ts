97ac8f392dd924ae343c69c63620a1acd12d8629
caspted <teddiejohn.rajeev26@gmail.com>
2024-05-10 22:45:32 +0800

    [Ted][Add] added wallet api routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -303,6 +303,46 @@ function routes(app: Express) {
       res.status(502).json("Certificate deleted successfully");
     }
   });
+
+  //Wallet API Routes
+  app.get("/wallets/:userId", async (req: Request, res: Response) => {
+    try {
+      const { userId } = req.params;
+
+      const wallet = await prisma.wallet.findUnique({
+        where: {
+          id: parseInt(userId),
+        },
+      });
+
+      if (!wallet) return res.status(404).json({ message: "Wallet Not found" });
+      res.status(200).json(wallet);
+    } catch {
+      res.status(200).json({ error: "Internal Server Error" });
+    }
+  });
+
+  app.post(
+    "/api/wallets/:userId/transactions",
+    async (req: Request, res: Response) => {
+      try {
+        const { balance, userId, user, transactions } = req.body;
+
+        const newTransaction = await prisma.wallet.create({
+          data: {
+            balance,
+            userId,
+            user,
+            transactions,
+          },
+        });
+
+        res.status(200).json(newTransaction);
+      } catch {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    },
+  );
 }
 
 export default routes;
