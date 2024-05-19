dd69536e7764b323cbf207bd8fd70d6dffb608bf
caspted <teddiejohn.rajeev26@gmail.com>
2024-05-19 12:16:05 +0800

    [Ted][Add] added transaction api routes

---
+++ backend/src/routes/transactions.ts
@@ -0,0 +1,53 @@
+import { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+function transactionRoutes(app: Express) {
+  
+  app.get("/api/transactions", async (req: Request, res: Response) => {
+    try {
+      const transactions = await prisma.transaction.findMany()
+      if (!transactions) res.status(404).json({ message: "There are no trasanctions made"})
+      res.status(200).json(transactions)
+    } catch {
+      res.status(500).json({ error: "Internat Server Error"})
+    }
+  })
+
+  app.get("/api/transactions/:userId", async (req: Request, res: Response) => {
+    try {
+      const { userId } = req.params
+      const userTransaction = await prisma.transaction.findUnique({
+        where: {
+          id: parseInt(userId)
+        }
+      })
+
+      if (!userTransaction) res.status(404).json( { message: "Transaction not found"})
+
+      res.status(200).json(userTransaction)
+    } catch {
+      res.status(500).json({ error: "Internal Server Error"})
+    }
+  })
+
+  app.post("/api/transactions", async (req: Request, res: Response) => {
+    try {
+      const { type, amount, wallet } = req.body
+      const newTransaction = await prisma.transaction.create({
+        data: {
+          type,
+          amount,
+          wallet
+        }
+      })
+
+      res.status(201).json(newTransaction)
+    } catch {
+      res.status(500).json({ error: "Internal Server Error"})
+    }
+  })
+}
+
+export default transactionRoutes;
